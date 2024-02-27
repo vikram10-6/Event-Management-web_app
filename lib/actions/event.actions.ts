fd2440a8aca41cf7268099a56475cb0bea764e25
vikram10-6 <vikramayer19@gmail.com>
2024-02-27 18:37:59 +0530

    added details in events redirecting page page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -6,7 +6,16 @@ import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
 import Category from "../database/models/category.model";
-import { json } from "stream/consumers";
+
+const populateEvent = async (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
 
 export const createEvent = async ({
   event,
@@ -22,10 +31,6 @@ export const createEvent = async ({
       throw new Error("Organizer not found");
     }
 
-    console.log({
-        categoryId: event.categoryId,
-        organizerId: userId
-    })
     const newEvent = await Event.create({
       ...event,
       Category: event.categoryId,
@@ -34,7 +39,22 @@ export const createEvent = async ({
 
     return JSON.parse(JSON.stringify(newEvent));
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    handleError(error);
+  }
+};
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+    if (!event) {
+      throw new Error("Event not found");
+    }
+
+    return JSON.parse(JSON.stringify(event));
+  } catch (error) {
     handleError(error);
   }
 };
